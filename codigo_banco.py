303bf0df24c4ffae749aac23eed2e7c5294a3516
estevan portilla <juanestevanportillamejia@gmail.com>
2025-07-05 16:42:06 -0500

    cajero

--- codigo_banco.py
+++ codigo_banco.py
@@ -7,7 +7,7 @@ historial=[]
 saldo=0
 
 #imprimo un mensaje de bienvenida para el ususari 
-print("bienvenido a banco aguila\n: por favor ingrese la carpeta")
+print("bienvenido a banco aguila\n: por favor ingrese la tarjeta")
 
 #solicito al usuario la contraseña (pero no la valida)
 contraseña=(input("ingrese la contraseña: "))
@@ -19,6 +19,7 @@ clave="ramon"
 while True :
     contraseña=(input("retifique la contraseña: "))
     if contraseña ==clave: 
+
         #comparo la contraseña ingresada con la clave correcta 
         print("¡contraseña correcta!")
         break
@@ -34,61 +35,81 @@ def bloque_especifico():
     
 #iniciamos el bucle principal del programa
 while True :
+
     #se le pide al usñuañrio una opcion 
     usu=(input("ingrese una opcion(salir / volver): "))
+
     #si la opcion es un numero valido, lo convierte en entero
     if usu.isdigit():
         usu = int(usu)
     if usu ==1:
+
         #muestra al usuario el saldo actual disponible en su cuenta
-        print(f"tu saldo actual es de :{saldo}")
+        print(f"tu saldo actual es de :{saldo}\n")
         print("gracias por visitar el banco aguila")
     elif usu ==2:
+
         #solicito al usuario el monto a depositar y el numero de cuenta
-        monto=(float(input("ingrese la contidad a depositar: ")))
+        monto=(int(input("ingrese la contidad a depositar: ")))
         numcuenta=(int(input("ingrese el numero de cuenta: ")))
+
          #calculo un pequeño interes sobre el monto (0,5%)
         interes=monto*0.005
+
         #realizo una operacion para restar el interes al monto
         monto_neto=monto-interes
+
         #sumo el monto al saldo actual
         saldo+=monto_neto
+
         #registro la operacion en la lista 
         historial.append(f"+{monto_neto} deposito")
+
         #muestro un mensaje indicando el numero d cuenta, monto depositado, interes, monto total y el saldo actual
-        print(f"se deposito en la cuenta, numero de cuenta : {numcuenta}\n  dinero depositado : {monto}\n menos el interes de '0,5%': {interes}\n totalidad ingresada en la cuenta: {monto_neto}\n el saldo actial : {saldo}")
+        print(f"se deposito en la cuenta, numero de cuenta : {numcuenta}\n  dinero depositado : {monto}\n menos el interes de '0,5%': {interes}\n totalidad ingresada en la cuenta: {monto_neto}\n el saldo actial : {saldo}\n")
         print("gracias por visitar el banco aguila")
     elif usu ==3:
+
         #solicito el monto a depositar y el numero de cuenta
-        monto=(float(input("ingrese la cantidad a retirar: ")))
+        monto=(int(input("ingrese la cantidad a retirar: ")))
         numcuenta=(int(input("ingrese el numero de cuenta: ")))
+
         #resto el monto retirdo al monto actual 
         saldo-=monto
+
         #registro la operacion en la lista
         historial.append(f"-{monto}retiro")
+
         #muestro un menu que indica el monto a retirar y el numero de cuenta
-        print(f"se han retirado {monto} \n en la cuenta {numcuenta}")
+        print(f"se han retirado {monto} \n en la cuenta {numcuenta}\n")
         print("gracias por visitar el banco aguila")
     elif usu ==4:
+
         #imprimo un encabezado
         print("historial de operaciones")
+
         #esto recorre la lista e imprime cada operacion 
         for h in historial:
             print(h)
+
             #imprimo el historial y el saldo actual
         print(f"el historial es : {historial}\n  el saldo es : {saldo}\n")
         print("gracias por visitar el banco aguila")
+
         #esto finaliza el proigrama si el usuario ingresa salir, imprimiendo el menzaje (programa finalizado...)
     elif usu=="salir":
-        print("programa finalizado...")
+        print("programa finalizado...\n")
         print("gracias por visitar el banco aguila")
         break
+
     #llama la funcion 'def' la cual imprime nuevamente el menu
     elif usu =="volver":
+
         #permite que el usuario vea de nuevo el menu sin salir de programa y continuar ejecutar nuevas opciones 
         bloque_especifico()
+
         #si la opcion ingresada por el usuario no es ninguna de las validas (1 ,2 ,3 ,4 , "salir", "volver") imprime un mensaje indicando que se produjo un error 
     else:
-        print("se produjo un error, gracias por utilizar banco aguila regrese en una hora")
-
+        print("se produjo un error")
+print("gracias porpreferir a nuestro banco S.A\n ¡que tenga buen dia!\n  Errrrr diablaz000")
     
